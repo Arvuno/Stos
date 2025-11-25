75053d18b9e0d2708dbda843aff019552ae04df5
Michał <michalfr1601@gmail.com>
2025-11-25 22:06:31 +0100

    logo added (probably temporary)

--- composeApp/src/jvmMain/kotlin/com/m4ykey/stos/main.kt
+++ composeApp/src/jvmMain/kotlin/com/m4ykey/stos/main.kt
@@ -4,14 +4,19 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.m4ykey.stos.app.App
 import com.m4ykey.stos.di.initModules
+import kmp_stos.composeapp.generated.resources.Res
+import kmp_stos.composeapp.generated.resources.logo
+import org.jetbrains.compose.resources.painterResource
 
 fun main() {
     initModules()
     application {
         Window(
             onCloseRequest = ::exitApplication,
-            title = "kmp-stos",
-            alwaysOnTop = true
+            title = "Stos",
+            alwaysOnTop = true,
+            resizable = true,
+            icon = painterResource(Res.drawable.logo)
         ) {
             App()
         }
